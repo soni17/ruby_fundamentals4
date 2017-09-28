f0819be05b5275b7be577410432badc3e94a5dd9
soni17 <elb004@gmail.com>
2017-09-27 22:32:19 -0400

    create array of trains

---
+++ exercise.rb
@@ -0,0 +1,15 @@
+require "pp"
+
+trains = [
+{train: "72C", frequency_in_minutes: 15, direction: "north"},
+{train: "72D", frequency_in_minutes: 15, direction: "south"},
+{train: "610", frequency_in_minutes: 5, direction: "north"},
+{train: "611", frequency_in_minutes: 5, direction: "south"},
+{train: "80A", frequency_in_minutes: 30, direction: "east"},
+{train: "80B", frequency_in_minutes: 30, direction: "west"},
+{train: "110", frequency_in_minutes: 15, direction: "north"},
+{train: "111", frequency_in_minutes: 15, direction: "south"}
+]
+
+pp trains
+
